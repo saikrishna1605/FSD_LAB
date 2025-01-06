042c5e34edcd1c927c10619d6d8f442b03c2b319
saikrishna1605 <saikrishnamukka6699@gmail.com>
2025-01-06 21:56:07 +0530

    Initial commit

---
+++ task1.js
@@ -0,0 +1,16 @@
+var a=10;
+let b=20;
+const c="Hello";
+console.log(a);
+console.log(b);
+console.log(c);
+
+a=50;
+b=60;
+console.log('Reassign value a :',a);
+console.log('Reassign value b :',b);
+
+var d="sai";
+var e="krishna";
+var f=1605;
+console.log(d+e+f);
