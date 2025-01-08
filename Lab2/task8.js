1bbed71ade662837955834a971c4a340d010b396
saikrishna1605 <saikrishnamukka6699@gmail.com>
2025-01-08 10:49:24 +0530

    task 8 completed

---
+++ Lab2/task8.js
@@ -0,0 +1,10 @@
+const colors = ['red', 'green', 'blue', 'yellow'];
+const [first, second] = colors;
+
+console.log(first);  
+console.log(second); 
+
+const [,sec,,fourth]=colors;
+
+console.log(sec);
+console.log(fourth); 
