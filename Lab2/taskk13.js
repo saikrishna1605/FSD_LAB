e720434d9d0d2139d11e125a7beed6cd823e7324
saikrishna1605 <saikrishnamukka6699@gmail.com>
2025-01-09 20:40:40 +0530

    Task 13 Done

---
+++ Lab2/taskk13.js
@@ -0,0 +1,6 @@
+const data = [{ name: 'John', age: 30 }, { name: 'Jane', age: 25 }];
+const [firstElement] = data;
+const { name, age } = firstElement;
+console.log(firstElement);
+console.log(name);
+console.log(age);
